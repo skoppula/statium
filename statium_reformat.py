d29630af96e748f510c4196e34f33a83944c6d4b
Skanda Koppula <skanda.koppula@gmail.com>
2014-01-19 19:22:25 -0500

    finished create_res and create_cfg functions

--- statium_reformat.py
+++ statium_reformat.py
@@ -1,9 +1,59 @@
-def create_res(in_pdb_path, out_res_path):
+#creates a simple cfg file from a *.res file to run statium analysis
+def create_cfg(in_res_path, out_cfg_path):
     
-    infile =  open(in_pdb_path, 'r')
-    outfile =  open(out_res_path, 'w')
+    infile = open(in_res_path, 'r')
+    outfile = open(out_cfg_path, 'w')
+    
     lines = infile.readlines()
+    outfile.write(lines[0][:-1] + "=0")
+    
+    infile.close()
+    outfile.close()
+        
 
+#Function to create *.res file: each line containing residue position to be considered in STATIUM analysis
+def create_res(in_pdb_path_orig, in_pdb_path_renumbered, out_res_path):
+    
+    infile_og =  open(in_pdb_path_orig, 'r')
+    infile_rn =  open(in_pdb_path_renumbered, 'r')
+    outfile =  open(out_res_path, 'w')
+    
+    lines_og = infile_og.readlines()
+    lines_rn = infile_rn.readlines()
+    
+    bchain_start_line = ""
+    num_residues = 0
+    init = 0
+    
+    for line in lines_og:
+        
+        if(line[21]=='B' and bchain_start_line == "" and line[0:4] =='ATOM'):
+            bchain_start_line = line
+            
+        elif(line[21]=='B' and line[0:3] =='TER' and bchain_start_line != ""):
+            num_residues = int(line[22:27]) - int(bchain_start_line[22:27]) + 1
+            break
+
+    for line in lines_rn:
+        #If AA identity and coordinates are the same, must be same atom and residue
+        if(line[17:21] == bchain_start_line[17:21] and line[32:55] == bchain_start_line[32:55]):
+            init = int(line[22:27])
+            break
+    
+    if(num_residues == 0):
+        print("Could not find a valid B-chain in: " + in_pdb_path_orig)
+        return
+    
+    for i in range(num_residues-1):
+        outfile.write(str(i+init) + '\n')
+        
+    outfile.write(str(num_residues-1+init)) #to ensure no newline at end of file
+    
+    outfile.close()
+    infile_og.close()
+    infile_rn.close()
+
+#Take a .pdb file from pdb.org and strip away meta-data so that output PDB only contains atoms and coordinates
 def renumber(start_res_num, start_atom_num, in_pdb_path, out_pdb_path):
    
     infile =  open(in_pdb_path, 'r')
--- statium_wrapper.py
+++ statium_wrapper.py
@@ -1,14 +1,15 @@
 import sys
 from docopt import docopt
 from statium_reformat import renumber
-from statium_reformat import create_res 
+from statium_reformat import create_res
+from statium_reformat import create_cfg 
 
 def main(argv):
     
     helpdoc =   """
                 usage: statium_wrapper.py renumber (IN_PDB) [OUT_PDB] [-v | --verbose]
-                       statium_wrapper.py create_res (IN_PDB) [OUT_RES] [-v | --verbose]
-                       statium_wrapper.py create_cfg (IN_RES) [-v | --verbose]
+                       statium_wrapper.py create_res (IN_PDB_ORIG IN_PDB_RENUMBERED) [OUT_RES] [-v | --verbose]
+                       statium_wrapper.py create_cfg (IN_RES) [OUT_CFG] [-v | --verbose]
                        statium_wrapper.py run_statium (IN_CFG IN_RES IN_PDB) [-v | --verbose]
                        statium_wrapper.py calc_seq_energy (DIR SEQ) [-v | --verbose]
                        statium_wrapper.py [-h | --help]
@@ -19,24 +20,38 @@ def main(argv):
     if(options['renumber']):
         if(options['-v'] or options['--verbose']): print("Reformatting file: " + options['IN_PDB'])
         
-        if(options['OUT_PDB'] == False):
+        if(options['OUT_PDB'] == None):
             renumber(1, 1, options['IN_PDB'], options['IN_PDB'][:-4]+'_renumbered.pdb')
             if(options['-v'] or options['--verbose']): print("Done. Formatted file: " + options['IN_PDB'][:-4]+'_renumbered.pdb')
             
         else:
             renumber(1, 1, options['IN_PDB'], options['OUT_PDB'])
             if(options['-v'] or options['--verbose']): print("Done. Formatted file: " + options['OUT_PDB'])
+    
         
     elif(options['create_res']):
-        if(options['-v'] or options['--verbose']): print("Creating .res file for: " + options['IN_PDB'])
+        if(options['-v'] or options['--verbose']): print("Creating .res file using: " + options['IN_PDB_ORIG'] + " and " + options['IN_PDB_RENUMBERED'])
         
-        if(options['OUT_RES'] == False):
-            create_res(1, 1, options['IN_PDB'], options['IN_PDB'][:-4]+'_renumbered.pdb')
-            if(options['-v'] or options['--verbose']): print("Done. .res file: " + options['IN_PDB'][:-4]+'.res')
+        if(options['OUT_RES'] == None):
+            create_res(options['IN_PDB_ORIG'], options['IN_PDB_RENUMBERED'], options['IN_PDB_RENUMBERED'][:-4]+'_renumbered.pdb')
+            if(options['-v'] or options['--verbose']): print("Done. .res file: " + options['IN_PDB_RENUMBERED'][:-4]+'.res')
             
         else:
-            create_res(1, 1, options['IN_PDB'], options['OUT_PDB'])
+            create_res(options['IN_PDB_ORIG'], options['IN_PDB_RENUMBERED'], options['OUT_RES'])
             if(options['-v'] or options['--verbose']): print("Done. .res file: " + options['OUT_RES'])
+    
+    
+    elif(options['create_cfg']):
+        if(options['-v'] or options['--verbose']): print("Creating .cfg file using: " + options['IN_RES'])
+        
+        if(options['OUT_CFG'] == None):   
+            create_cfg(options['IN_RES'], options['IN_RES'][:-4]+'.cfg')
+            if(options['-v'] or options['--verbose']): print("Done. .cfg file: " + options['IN_RES'][:-4]+'.cfg')
+            
+        else:
+            create_cfg(options['IN_RES'], options['OUT_CFG'])
+            if(options['-v'] or options['--verbose']): print("Done. .cfg file: " + options['OUT_CFG'])
+        
         
     elif(options['run_statium']):
         if(options['-v'] or options['--verbose']): print("Running STATIUM with: " + options['IN_CFG'] + " " + options['IN_RES'] + " " + options['IN_PDB'])
