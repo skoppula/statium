dd3b57192751c1d23944ba855ac099835aa0ff71
Skanda Koppula <skanda.koppula@gmail.com>
2014-01-21 20:29:34 -0500

    begin statium analysis functionality

---
+++ statium_analysis.py
@@ -0,0 +1,10 @@
+def find_counts(in_cfg_path, in_res_path, in_pdb_path, in_pdb_lib_dir, out_dir, verbose):
+    print("HELLO!")
+
+
+
+#                 if sys.argv[i] == '-build_statium':
+#         preset = sys.argv[i + 1]
+#                 statium_sidechain_coyote(preset, 'REG', 5000, sys.argv[0])
+#                 statium_sidechain_coyote_compile(preset)
+#         convert_counts_sidechain(preset)
--- statium_wrapper.py
+++ statium_wrapper.py
@@ -2,7 +2,8 @@ import sys
 from docopt import docopt
 from statium_reformat import renumber
 from statium_reformat import create_res
-from statium_reformat import create_cfg 
+from statium_reformat import create_cfg
+from statium_analysis import find_counts
 
 def main(argv):
     
@@ -10,54 +11,64 @@ def main(argv):
                 usage: statium_wrapper.py renumber (IN_PDB) [OUT_PDB] [-v | --verbose]
                        statium_wrapper.py create_res (IN_PDB_ORIG IN_PDB_RENUMBERED) [OUT_RES] [-v | --verbose]
                        statium_wrapper.py create_cfg (IN_RES) [OUT_CFG] [-v | --verbose]
-                       statium_wrapper.py run_statium (IN_CFG IN_RES IN_PDB) [-v | --verbose]
+                       statium_wrapper.py run_statium (IN_CFG IN_RES IN_PDB IN_PDB_LIB_DIR) [OUT_DIR] [-v | --verbose]
                        statium_wrapper.py calc_seq_energy (DIR SEQ) [-v | --verbose]
                        statium_wrapper.py [-h | --help]
                 """
     
     options = docopt(helpdoc, argv, help = True, version = "2.0.0", options_first=False)
+    verbose = options['-v'] or options['--verbose']
     
     if(options['renumber']):
-        if(options['-v'] or options['--verbose']): print("Reformatting file: " + options['IN_PDB'])
+        if(verbose): print("Reformatting file: " + options['IN_PDB'])
         
         if(options['OUT_PDB'] == None):
             renumber(1, 1, options['IN_PDB'], options['IN_PDB'][:-4]+'_renumbered.pdb')
-            if(options['-v'] or options['--verbose']): print("Done. Formatted file: " + options['IN_PDB'][:-4]+'_renumbered.pdb')
+            if(verbose): print("Done. Formatted file: " + options['IN_PDB'][:-4]+'_renumbered.pdb')
             
         else:
             renumber(1, 1, options['IN_PDB'], options['OUT_PDB'])
-            if(options['-v'] or options['--verbose']): print("Done. Formatted file: " + options['OUT_PDB'])
+            if(verbose): print("Done. Formatted file: " + options['OUT_PDB'])
     
         
     elif(options['create_res']):
-        if(options['-v'] or options['--verbose']): print("Creating .res file using: " + options['IN_PDB_ORIG'] + " and " + options['IN_PDB_RENUMBERED'])
+        if(verbose): print("Creating .res file using: " + options['IN_PDB_ORIG'] + " and " + options['IN_PDB_RENUMBERED'])
         
         if(options['OUT_RES'] == None):
             create_res(options['IN_PDB_ORIG'], options['IN_PDB_RENUMBERED'], options['IN_PDB_RENUMBERED'][:-4]+'_renumbered.pdb')
-            if(options['-v'] or options['--verbose']): print("Done. .res file: " + options['IN_PDB_RENUMBERED'][:-4]+'.res')
+            if(verbose): print("Done. .res file: " + options['IN_PDB_RENUMBERED'][:-4]+'.res')
             
         else:
             create_res(options['IN_PDB_ORIG'], options['IN_PDB_RENUMBERED'], options['OUT_RES'])
-            if(options['-v'] or options['--verbose']): print("Done. .res file: " + options['OUT_RES'])
+            if(verbose): print("Done. .res file: " + options['OUT_RES'])
     
     
     elif(options['create_cfg']):
-        if(options['-v'] or options['--verbose']): print("Creating .cfg file using: " + options['IN_RES'])
+        if(verbose): print("Creating .cfg file using: " + options['IN_RES'])
         
         if(options['OUT_CFG'] == None):   
             create_cfg(options['IN_RES'], options['IN_RES'][:-4]+'.cfg')
-            if(options['-v'] or options['--verbose']): print("Done. .cfg file: " + options['IN_RES'][:-4]+'.cfg')
+            if(verbose): print("Done. .cfg file: " + options['IN_RES'][:-4]+'.cfg')
             
         else:
             create_cfg(options['IN_RES'], options['OUT_CFG'])
-            if(options['-v'] or options['--verbose']): print("Done. .cfg file: " + options['OUT_CFG'])
+            if(verbose): print("Done. .cfg file: " + options['OUT_CFG'])
         
         
     elif(options['run_statium']):
-        if(options['-v'] or options['--verbose']): print("Running STATIUM with: " + options['IN_CFG'] + " " + options['IN_RES'] + " " + options['IN_PDB'])
         
+        if(verbose): print("Running STATIUM with: " + options['IN_CFG'] + " " + options['IN_RES'] + " " + options['IN_PDB'])
+        
+        if(options['OUT_DIR'] == None):   
+            find_counts(options['IN_CFG'], options['IN_RES'], options['IN_PDB'], options['IN_PDB_LIB_DIR'], options['IN_CFG'][:-4], verbose)
+            if(verbose): print("Done. STATIUM probabilities in output directory: " + options['IN_CFG'][:-4]);
+            
+        else:
+            find_counts(options['IN_CFG'], options['IN_RES'], options['IN_PDB'], options['IN_PDB_LIB_DIR'], options['OUT_DIR'], verbose)
+            if(verbose): print("Done. STATIUM probabilities in output directory: " + options['OUT_DIR']);
+
     elif(options['calc_seq_energy']):
-        if(options['-v'] or options['--verbose']): print("Calculating energy for sequence: " + options['SEQ'] + " with STATIUM output directory " + options['DIR'])
+        if(verbose): print("Calculating energy for sequence: " + options['SEQ'] + " with STATIUM output directory " + options['DIR'])
         
 if __name__ == "__main__":
     main(sys.argv[1:])
