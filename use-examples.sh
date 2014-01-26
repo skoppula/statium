30f37ddfb44980bbb869bbcebe86927a88154538
Skanda Koppula <skanda.koppula@gmail.com>
2014-01-26 06:02:51 -0500

    adding use examples in shell script

---
+++ use-examples.sh
@@ -0,0 +1,11 @@
+#                usage: statium_wrapper.py renumber (IN_PDB) [OUT_PDB] [-v | --verbose]
+#                       statium_wrapper.py create_res (IN_PDB_ORIG IN_PDB_RENUMBERED) [OUT_RES] [-v | --verbose]
+#                       statium_wrapper.py run_statium (IN_RES IN_PDB IN_PDB_LIB_DIR IN_IP_LIB_DIR) [OUT_DIR] [-v | --verbose]
+#                       statium_wrapper.py calc_seq_energy (IN_RES IN_DIR PROBS_DIR SEQ) [-v | --verbose]
+#                       statium_wrapper.py [-h | --help]
+
+#an example:
+python statium_wrapper.py renumber testing/4hfz_orig.pdb testing/4hfz.pdb -v
+python statium_wrapper.py create_res testing/4hfz_orig.pdb testing/4hfz.pdb testing/4hfz.res -v
+python statium_wrapper.py run_statium testing/4hfz.res testing/4hfz.pdb culled_90_reduced/ ip_90_wGLY/ testing/output -v
+python statium_wrapper.py calc_seq_energy testing/4hfz.res testing/output/ testing/output_probs/ ETFSDLWKLLP
