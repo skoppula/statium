1a0e8606a925caa939bda0e1b4417a3900d4e6aa
Skanda Koppula <skanda.koppula@gmail.com>
2014-07-14 17:27:12 -0400

    cleanup

--- testing/old-results/2vm6/old-statium/results/bfl1_2vm6_coyote/seq_1.sh
+++
@@ -1,2 +0,0 @@
-#PBS -S /bin/sh
-./build_test.py -statium_sidechain bfl1_2vm6 bfl1_2vm6_coyote/seq_1.txt bfl1_2vm6_coyote 1
--- testing/old-results/4hfz/old-statium/results-and-env/4hfz_coyote/seq_1.sh
+++
@@ -1,2 +0,0 @@
-#PBS -S /bin/sh
-./build_test.py -statium_sidechain 4hfz 4hfz_coyote/seq_1.txt 4hfz_coyote 1
