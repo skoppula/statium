0169a5a994d6fa070baae670929af728cd6f2936
Skanda Koppula <skoppula@mit.edu>
2014-08-21 00:32:13 -0400

    removing data

--- c-modules/rmsd/extensions.cpp
+++ c-modules/rmsd/extensions.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
+#include <string>
+#include <assert>
+#include <stdlib>
+#include <stdio>
 #include "strtk.hpp"
  
 using namespace std;
- 
+
+char** str_split(char* a_str, const char a_delim) {
+	char** result = 0;
+	size_t count = 0;
+	char* tmp = a_str;
+	char* last_comma = 0;
+	char delim[2];
+	delim[0] = a_delim;
+	delim[1] = 0;
+
+	/* Count how many elements will be extracted. */
+	while (*tmp) {
+		if (a_delim == *tmp) {
+				count++;
+				last_comma = tmp;
+		}
+		tmp++;
+	}
+
+	/* Add space for trailing token. */
+	count += last_comma < (a_str + strlen(a_str) - 1);
+
+	/* Add space for terminating null string so caller
+	 * 	knows where the list of returned strings ends. */
+	count++;
+
+	result = malloc(sizeof(char*) * count);
+
+	if (result) {
+		size_t idx  = 0;
+		char* token = strtok(a_str, delim);
+
+		while (token) {
+			assert(idx < count);
+			*(result + idx++) = strdup(token);
+			token = strtok(0, delim);
+		}
+		assert(idx == count - 1);
+		*(result + idx) = 0;
+	}
+
+	return result;
+}
+
 void query_distance(const char* libfile, const char* pairs, const char* values) {
 	//std::string int_string = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
 	//std::vector<int> int_list;
